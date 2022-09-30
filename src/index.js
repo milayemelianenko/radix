34de4dcb05526facc54b393555235b76a0aa40ae
Milauraaa <mila@stealth.design>
2022-09-30 13:09:48 +0100

    added javascript and fixed bugs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
-function openForm() {
-  document.getElementById("form-section").style.display = "block";
-  "bg-section".toggleClass("shadow");
-}
-function closedForm() {
-  document.getElementById("form-section").style.display = "none";
-}
+// const openBtn = document.querySelector("#open-btn");
+// const modal = document.querySelector("#form-section-frame");
+// const closeBtn = document.querySelector("#cancel-btn");
+
+// let disableScroll = function () {
+//   let pagePosition = window.scrollY;
+//   document.body.classList.add("disable-scroll");
+//   document.body.dataset.position = pagePosition;
+//   document.body.style.top = -pagePosition + "px";
+// };
+
+// let enableScroll = function () {
+//   let pagePosition = parseInt(document.body.dataset.position, 10);
+//   document.body.style.top = "auto";
+//   document.body.classList.remove("disable-scroll");
+//   window.scroll({ top: pagePosition, left: 0 });
+//   document.body.removeAttribute("data-position");
+// };
+
+// openBtn.addEventListener("click", () => {
+//   modal.style.display = "block";
+//   disableScroll();
+// });
+
+// modal.addEventListener("click", (e) => {
+//   if (e.target == modal) {
+//     modal.style.display = "none";
+//     enableScroll();
+//   }
+// });
+
+// closeBtn.addEventListener("click", () => {
+//   modal.style.display = "none";
+//   enableScroll();
+// });
