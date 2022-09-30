e081b78fcfac554fdfe9db39ce778b63861701cf
Milauraaa <mila@stealth.design>
2022-09-30 07:40:26 +0100

    build website

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function openForm() {
+  document.getElementById("form-section").style.display = "block";
+  "bg-section".toggleClass("shadow");
+}
+function closedForm() {
+  document.getElementById("form-section").style.display = "none";
+}
